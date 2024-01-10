0daf60d9c125d3fff702847b51d32534c5f4c2d2
Uhunoma Paul <127848046+Codinplus31@users.noreply.github.com>
2024-01-10 16:04:01 +0100

    Create xjsx-router.js

---
+++ modules/xjsx-router.js
@@ -0,0 +1,90 @@
+(function() {
+  var __core__ = XJSX.__XJSXCORE__();
+  var FUNCTION = 0xC;
+  let routeritem = [];
+    /** print √ **/
+    __core__.createModule([
+      {
+        keyword: "xjsx-router",
+        onload:function(arg,node){
+          let router;
+        [this.global.router = router = arg]
+        
+        },
+        onprogress:function(arg,node){
+          
+        },
+        callback: function(e, node, exec) {
+
+          try {
+            routeritem.push({routes:this.global.router.split("'").join('').trim(),dom: this.dom()})
+            var oldhash = location.hash === ''? '/': '/'+location.hash;
+            let self = this;
+          
+              
+          let oldrouter =   routeritem.filter((e)=>{
+      
+                if(e.routes === oldhash ){
+                  return e;
+                
+              }
+            
+              })
+          let errorrouter =   routeritem.filter((e)=>{
+      
+                if(e.routes === '' || e.routes === '*'){
+                  return e;
+                
+              }
+            
+              })
+              if(oldrouter.length === 0 && errorrouter.length === 0){
+                this.putChild('')
+              }else if(oldrouter.length === 0 && errorrouter.length > 0){
+                errorrouter.map((e)=>{
+                  self.putChild(e.dom[0])
+                })
+              }
+              else{
+
+                oldrouter.map((e)=>{
+                  self.putChild(e.dom[0])
+                })
+              }
+              
+            
+          window.onhashchange = function(s){
+            let newRout = '/'+s.newURL.split('').splice(s.newURL.split('').indexOf('#'),s.newURL.split('').length -1).join('')
+                    let newRouter = routeritem.filter((e)=>{
+                      if(e.routes === newRout){
+                      
+                        return e;
+                      }else{
+                      }
+                    })
+
+                    if(newRouter.length === 0 && errorrouter.length === 0){
+                      self.putChild('')
+                    }else if(newRouter.length === 0 && errorrouter.length > 0){
+                      errorrouter.map((e)=>{
+                        self.putChild(e.dom[0])
+                      })
+                    }
+                    else{
+      
+                      newRouter.map((e)=>{
+                        self.putChild(e.dom[0])
+                      })
+                    }
+
+          }
+          
+          } catch (err) {
+            console.error("router:", e, err + "");
+            e = "";
+          }
+        },
+        type: FUNCTION,
+      },
+    ]);
+  })()  
